d379f82418bd2853ef6b24a8470c5e809b978691
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-09 14:37:20 +0800

    修复支付管理搜索，查看详情页面样式

--- functions/global.func.php
+++ functions/global.func.php
@@ -74,16 +74,15 @@ function get_payment_record_list($args = array()) {
 	
     $db_payment_record = RC_DB::table('payment_record');
     $filter = array();
-    $filter['order_sn']		= empty($args['order_sn'])		? ''		: intval($args['order_sn']);
-    $filter['trade_no']		= empty($args['trade_no'])			? 0			: intval($args['trade_no']);
+    $filter['order_sn']		= empty($args['order_sn'])		? ''		: trim($args['order_sn']);
+    $filter['trade_no']		= empty($args['trade_no'])		? 0			: trim($args['trade_no']);
     $filter['pay_status']	= $args['pay_status'];
     
-
     if ($filter['order_sn']) {
         $db_payment_record->where('order_sn', 'LIKE', '%' . mysql_like_quote($filter['order_sn']) . '%');
     }
     if ($filter['trade_no']) {
-        $db_payment_record->where('trade_no', 'LIKE', '%' . mysql_like_quote($filter['trade_no']) .'%');
+        $db_payment_record->where('trade_no', 'LIKE', '%' . mysql_like_quote($filter['trade_no']) . '%');
     }
 
     
--- templates/admin/payment_record_info.dwt.php
+++ templates/admin/payment_record_info.dwt.php
@@ -13,21 +13,19 @@
     </h3>
 </div>
 
-<form name="moneyForm" action='{url path="payment/admin_payment_record/info" args="step={$step}&order_sn={$order_sn}&step_act={$step_act}"}' method="post">
-    <div class="row-fluid">
-        <div class="span12">
-            <div class="form-inline foldable-list">
-
-                <div class="accordion-group">
-                    <div class="accordion-heading">
-                        <a class="accordion-toggle acc-in" data-toggle="collapse" data-target="#collapseOne"><strong>{lang key='payment::payment.fund_flow_record'}</strong></a>
-                    </div>
+<div class="row-fluid">
+    <div class="span12">
+        <div class="form-inline foldable-list">
+            <div class="accordion-group">
+                <div class="accordion-heading">
+                    <a class="accordion-toggle acc-in" data-toggle="collapse" data-target="#collapseOne"><strong>{lang key='payment::payment.fund_flow_record'}</strong></a>
+                </div>
                 <div class="accordion-body in in_visable collapse" id="collapseOne">
                     <table class="table table-oddtd m_b0">
                         <tbody class="first-td-no-leftbd">
                         <tr>
                             <td><div align="right"><strong>{lang key='payment::payment.order_sn'}</strong></div></td>
-                            <td><a href='{url path="/orders/admin/info" args="order_id={$order.order_id}"}'>{$modules.order_sn}</a></td>
+                            <td><a target="__blank" href='{url path="/orders/admin/info" args="order_id={$order.order_id}"}'>{$modules.order_sn}</a></td>
                             <td><div align="right"><strong>{lang key='payment::payment.pay_status'}</strong></div></td>
                             <td>{$modules.pay_status}</td>
                         </tr>
@@ -56,59 +54,57 @@
                         </tbody>
                     </table>
                 </div>
+            </div>
 
-                <!-- {if $check_modules} -->
-                    <div class="accordion-group">
-                        <div class="accordion-heading">
-                            <a class="accordion-toggle acc-in" data-toggle="collapse" data-target="#collapseTwo"><strong>{lang key='payment::payment.heading_order_info'}</strong></a>
-                        </div>
-                    </div>
-                    <div class="accordion-body in in_visable collapse" id="collapseTwo">
-                        <table class="table table-oddtd m_b0">
-                            <tbody class="first-td-no-leftbd">
-                            <tr>
-                                <td><div align="right"><strong>{lang key='orders::order.label_goods_amount'}</strong></div></td>
-                                <td>{$order.formated_goods_amount}</td>
-                                <td><div align="right"><strong>{lang key='orders::order.label_discount'}</strong></div></td>
-                                <td>{$order.discount}</td>
-                            </tr>
-                            <tr>
-                                <td><div align="right"><strong>{lang key='orders::order.label_tax'}</strong></div></td>
-                                <td>{$order.tax}</td>
-                                <td><div align="right"><strong>{lang key='orders::order.label_order_amount'}</strong></div></td>
-                                <td>{$order.formated_total_fee}</td>
-                            </tr>
-                            <tr>
-                                <td><div align="right"><strong>{lang key='orders::order.label_shipping_fee'}</strong></div></td>
-                                <td>{$order.shipping_fee}</td>
-                                <td><div align="right"><strong>{lang key='orders::order.label_money_paid'}</strong></div></td>
-                                <td>{$order.formated_money_paid} </td>
-                            </tr>
-                            <tr>
-                                <td><div align="right"><strong>{lang key='orders::order.label_insure_fee'}</strong></div></td>
-                                <td>{if $exist_real_goods}{else}0{/if}</td>
-                                <td><div align="right"><strong>{lang key='orders::order.label_surplus'}</strong></div></td>
-                                <td>{$order.surplus}</td>
-                            <tr>
-                                <td><div align="right"><strong>{lang key='orders::order.label_pay_fee'}</strong></div></td>
-                                <td>{$order.pay_fee}</td>
-                                <td><div align="right"><strong>{lang key='orders::order.label_card_fee'}</strong></div></td>
-                                <td>{$order.card_fee}</td>
-                            </tr>
-                            <td><div align="right"><strong>{lang key='orders::order.label_pack_fee'}</strong></div></td>
-                            <td>{$order.pack_fee}</td>
-                            <td><div align="right"><strong>{if $order.order_amount >= 0} {lang key='orders::order.label_money_dues'} {else} {lang key='orders::order.label_money_refund'} {/if}</strong></div></td>
-                            <td>{$order.formated_order_amount}</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-        <!-- {/if} -->
-            </div>
-            </div>
+            <!-- {if $check_modules} -->
+			<div class="accordion-group">
+				<div class="accordion-heading">
+				    <a class="accordion-toggle acc-in" data-toggle="collapse" data-target="#collapseTwo"><strong>{lang key='payment::payment.heading_order_info'}</strong></a>
+				</div>
+				<div class="accordion-body in in_visable collapse" id="collapseTwo">
+				    <table class="table table-oddtd m_b0">
+				        <tbody class="first-td-no-leftbd">
+				        <tr>
+				            <td><div align="right"><strong>{lang key='orders::order.label_goods_amount'}</strong></div></td>
+				            <td>{$order.formated_goods_amount}</td>
+				            <td><div align="right"><strong>{lang key='orders::order.label_discount'}</strong></div></td>
+				            <td>{$order.discount}</td>
+				        </tr>
+				        <tr>
+				            <td><div align="right"><strong>{lang key='orders::order.label_tax'}</strong></div></td>
+				            <td>{$order.tax}</td>
+				            <td><div align="right"><strong>{lang key='orders::order.label_order_amount'}</strong></div></td>
+				            <td>{$order.formated_total_fee}</td>
+				        </tr>
+				        <tr>
+				            <td><div align="right"><strong>{lang key='orders::order.label_shipping_fee'}</strong></div></td>
+				            <td>{$order.shipping_fee}</td>
+				            <td><div align="right"><strong>{lang key='orders::order.label_money_paid'}</strong></div></td>
+				            <td>{$order.formated_money_paid} </td>
+				        </tr>
+				        <tr>
+				            <td><div align="right"><strong>{lang key='orders::order.label_insure_fee'}</strong></div></td>
+				            <td>{if $exist_real_goods}{else}0{/if}</td>
+				            <td><div align="right"><strong>{lang key='orders::order.label_surplus'}</strong></div></td>
+				            <td>{$order.surplus}</td>
+				        <tr>
+				            <td><div align="right"><strong>{lang key='orders::order.label_pay_fee'}</strong></div></td>
+				            <td>{$order.pay_fee}</td>
+				            <td><div align="right"><strong>{lang key='orders::order.label_card_fee'}</strong></div></td>
+				            <td>{$order.card_fee}</td>
+				        </tr>
+				        <tr>
+				            <td><div align="right"><strong>{lang key='orders::order.label_pack_fee'}</strong></div></td>
+				            <td>{$order.pack_fee}</td>
+				            <td><div align="right"><strong>{if $order.order_amount >= 0} {lang key='orders::order.label_money_dues'} {else} {lang key='orders::order.label_money_refund'} {/if}</strong></div></td>
+				            <td>{$order.formated_order_amount}</td>
+				        </tr>
+				        </tbody>
+				    </table>
+				</div>
+			</div>
+    		<!-- {/if} -->
         </div>
     </div>
-</form>
-
+</div>
 <!-- {/block} -->
