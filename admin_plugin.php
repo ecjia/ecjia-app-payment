4ad3424bbcbe082431adddc01c09f07e16ea583a
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-05-24 17:45:36 +0800

    增加插件列表菜单高亮

--- admin_plugin.php
+++ admin_plugin.php
@@ -71,6 +71,7 @@ class admin_plugin extends ecjia_admin {
 		
 		RC_Loader::load_app_class('payment_factory', null, false);
 		ecjia_screen::get_current_screen()->add_nav_here(new admin_nav_here(__('支付方式'), RC_Uri::url('payment/admin_plugin/init')));
+		ecjia_screen::get_current_screen()->set_parentage('payment', 'payment/admin_plugin.php');
 	}
 
 	/**
