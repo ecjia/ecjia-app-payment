c81be66f2023ab15600aa6120ef9197090c2afb9
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-07-19 19:14:28 +0800

    更新支付应用内接口的错误提示

--- apis/payment_update_payment_record_api.class.php
+++ apis/payment_update_payment_record_api.class.php
@@ -59,7 +59,7 @@ class payment_update_payment_record_api extends Component_Event_Api {
      */
 	public function call(&$options) {	
 		if (! array_get($options, 'order_sn') || ! array_has($options, 'trade_no')) {
-			return new ecjia_error('invalid_parameter', RC_Lang::get('payment::payment.invalid_parameter'));
+			return new ecjia_error('invalid_parameter', __('缺少必要参数'));
 		}
 		
 		/* 插入支付流水记录 */
