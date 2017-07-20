b40eca06314eeaaa81ef1b5226b0ccbaec9f7854
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-07-20 17:45:57 +0800

    更新门店微信支付方式

--- modules/order/pay_module.class.php
+++ modules/order/pay_module.class.php
@@ -125,7 +125,7 @@ class pay_module extends api_front implements api_interface {
         }
         $payment_list = RC_Api::api('payment', 'available_payments', array('store_id' => $order['store_id'], 'cod_fee' => $cod_fee));
         
-        $other = collect($payment_list)->filter(function ($item) use ($order) {
+        $other = collect($payment_list)->mapWithKeys(function ($item) use ($order) {
         	if ($item['pay_id'] == $order['pay_id']) {
         	    return false;
         	}
@@ -135,7 +135,7 @@ class pay_module extends api_front implements api_interface {
         	return $item;
         })->toArray();
         
-        return array('payment' => $order['payment'], 'other' => $other);
+        return array('payment' => $order['payment'], 'others' => $other);
 	}
 }
 
