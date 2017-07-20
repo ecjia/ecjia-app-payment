4f624efc03a64ebccf1cdcdb94386948cc539913
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-07-20 19:02:24 +0800

    更新门店微信支付方式

--- modules/order/pay_module.class.php
+++ modules/order/pay_module.class.php
@@ -136,7 +136,7 @@ class pay_module extends api_front implements api_interface {
         _dump($payment_list);
         $payments = collect($payment_list)->mapWithKeys(function ($item) use ($order) {
             if ($item['pay_id'] == $order['pay_id']) {
-                return false;
+                return [];
             }
 
             $item['pay_name'] = strip_tags($item['pay_name']).'x';
