edd357f2b8549786ab5daaa1d58307e3df57770b
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-08-24 18:17:55 +0800

    移除注释

--- classes/PaymentAbstract.php
+++ classes/PaymentAbstract.php
@@ -203,7 +203,6 @@ abstract class PaymentAbstract extends AbstractPlugin
         }
         
         if (! is_ecjia_error($result)) {
-            \RC_Logger::getLogger('pay')->info('order_payed_do_something');
             RC_Hook::do_action('order_payed_do_something', $item['order_sn']); 
         }
         
