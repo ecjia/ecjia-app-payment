c5eccace5432282fee165bb98d9fa154b9c70fd0
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-11 18:39:20 +0800

    为所有的APP添加ServiceProvider

--- classes/PaymentServiceProvider.php
+++ classes/PaymentServiceProvider.php
@@ -46,15 +46,18 @@
 //
 namespace Ecjia\App\Payment;
 
-use Royalcms\Component\Support\ServiceProvider;
+use Royalcms\Component\App\AppServiceProvider;
 
-class PaymentServiceProvider extends ServiceProvider {
+class PaymentServiceProvider extends AppServiceProvider 
+{
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot() {
+    public function boot() 
+    {
+        $this->package('ecjia/app-payment');
     }
 
 	/**
