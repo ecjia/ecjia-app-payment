7f979ed38de3f462a95f3c334db7e0aa491eb17f
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-10-29 18:13:51 +0800

    修正扫码支付接口

--- modules/admin/payment/scancode_module.class.php
+++ modules/admin/payment/scancode_module.class.php
@@ -24,7 +24,7 @@ class admin_payment_scancode_module extends api_admin implements api_interface
      *
      * @param \Royalcms\Component\Http\Request $request
      */
-    public function handleRequest(\Royalcms\Component\Http\Request $request)
+    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request)
     {
         if ($_SESSION['admin_id'] <= 0 && $_SESSION['staff_id'] <= 0) {
             return new ecjia_error(100, 'Invalid session');
