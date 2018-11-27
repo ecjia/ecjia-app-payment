8294f33678299e737eb33d03b136f893232d9be0
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-11-27 15:52:13 +0800

    统一退款封装方法名

--- classes/Refund/CancelManager.php
+++ classes/Refund/CancelManager.php
@@ -9,55 +9,17 @@
 namespace Ecjia\App\Payment\Refund;
 
 use Ecjia\App\Payment\Contracts\CancelPayment;
-use Ecjia\App\Payment\Repositories\PaymentRecordRepository;
-use Ecjia\App\Payment\PaymentPlugin;
 use ecjia_error;
 
-class CancelManager
+class CancelManager extends RefundAbstract
 {
-    protected $order_sn;
 
-    protected $pay_code;
-
-    protected $plugin_handler;
-
-    protected $payment_record;
-
-    public function __construct($order_sn)
-    {
-        $this->order_sn = $order_sn;
-    }
-
-    public function cancel($order_trade_no = null)
-    {
-        $paymentRecordRepository = new PaymentRecordRepository();
-
-        if (is_null($order_trade_no)) {
-            $this->record_model = $paymentRecordRepository->findBy('order_trade_no', $order_trade_no);
-        } else {
-            $this->record_model = $paymentRecordRepository->findBy('order_sn', $this->order_sn);
-        }
-
-        if (empty($this->record_model)) {
-            return new ecjia_error('payment_record_not_found', __('此笔交易记录未找到', 'app-payment'));
-        }
-
-        $this->pay_code = $this->record_model->pay_code;
-
-        $payment_plugin	= new PaymentPlugin();
-        $this->plugin_handler = $payment_plugin->channel($this->pay_code);
-        if (is_ecjia_error($this->plugin_handler))
-        {
-            return $this->plugin_handler;
-        }
-
-        $this->plugin_handler->setPaymentRecord($paymentRecordRepository);
-
-        return $this->cancelPluginHandler();
-    }
-
-
-    protected function cancelPluginHandler()
+    /**
+     * 退款插件处理
+     *
+     * @return array|ecjia_error
+     */
+    protected function refundPluginHandler()
     {
         if (! ($this->plugin_handler instanceof CancelPayment)) {
             return new ecjia_error('payment_plugin_not_support__cancel_payment', $this->plugin_handler->getName().'支付方式不支持退款操作');
---
+++ classes/Refund/RefundAbstract.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2018/11/27
+ * Time: 15:46
+ */
+
+namespace Ecjia\App\Payment\Refund;
+
+use Ecjia\App\Payment\Repositories\PaymentRecordRepository;
+use Ecjia\App\Payment\PaymentPlugin;
+use ecjia_error;
+
+abstract class RefundAbstract
+{
+
+    protected $order_sn;
+
+    protected $pay_code;
+
+    protected $plugin_handler;
+
+    protected $payment_record;
+
+    public function __construct($order_sn)
+    {
+        $this->order_sn = $order_sn;
+    }
+
+    public function refund($order_trade_no = null)
+    {
+        $paymentRecordRepository = new PaymentRecordRepository();
+
+        if (is_null($order_trade_no)) {
+            $this->record_model = $paymentRecordRepository->findBy('order_trade_no', $order_trade_no);
+        } else {
+            $this->record_model = $paymentRecordRepository->findBy('order_sn', $this->order_sn);
+        }
+
+        if (empty($this->record_model)) {
+            return new ecjia_error('payment_record_not_found', __('此笔交易记录未找到', 'app-payment'));
+        }
+
+        $this->pay_code = $this->record_model->pay_code;
+
+        $payment_plugin	= new PaymentPlugin();
+        $this->plugin_handler = $payment_plugin->channel($this->pay_code);
+        if (is_ecjia_error($this->plugin_handler))
+        {
+            return $this->plugin_handler;
+        }
+
+        $this->plugin_handler->setPaymentRecord($paymentRecordRepository);
+
+        return $this->refundPluginHandler();
+    }
+
+    /**
+     * 退款插件处理
+     *
+     * @return mixed
+     */
+    abstract protected function refundPluginHandler();
+
+}
