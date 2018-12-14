89e6539d459e0075f5bf7d57ab717a93ccb4eae9
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-12-14 16:35:31 +0800

    admin/payment/notify/pay修改

--- modules/admin/payment/notify/pay_module.class.php
+++ modules/admin/payment/notify/pay_module.class.php
@@ -70,7 +70,7 @@ class admin_payment_notify_pay_module extends api_admin implements api_interface
         
         //传参判断
         if (empty($order_trade_no) || empty($notify_data)) {
-        	return new ecjia_error( 'invalid_parameter', 'admin_payment_notify_pay_module接口参数无效');
+        	return new ecjia_error('invalid_parameter', 'admin_payment_notify_pay_module接口参数无效');
         }
         
         //查找交易记录
