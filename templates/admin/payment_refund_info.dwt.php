f06349d34e203f77e539cbb96f66a93ede4b488b
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-01-07 12:16:19 +0800

    修改url

--- templates/admin/payment_refund_info.dwt.php
+++ templates/admin/payment_refund_info.dwt.php
@@ -35,7 +35,7 @@
                             <td><div align="right"><strong>退款状态</strong></div></td>
                             <td>
                                 {$payment_refund_info.label_refund_status}
-                                <a class="btn m_l5 payrecord_query" href="javascript:;" data-url="{RC_Uri::url('refund/admin_payment_refund/query')}&id={$payment_refund_info.id}">对账查询</a>
+                                <a class="btn m_l5 payrecord_query" href="javascript:;" data-url="{RC_Uri::url('payment/admin_payment_refund/query')}&id={$payment_refund_info.id}">对账查询</a>
                             </td>
                         </tr>
                         <tr>
